5c4c2f32442495054ff38afc59af970441444062
kmaznitsyn <maznichin03@gmail.com>
2022-12-10 22:05:43 +0200

    Impl map based interfaces. closes #6.

---
+++ pet-clinic-data/src/main/java/udemy/studying/petclinic/services/map/PetServiceMap.java
@@ -0,0 +1,33 @@
+package udemy.studying.petclinic.services.map;
+
+import udemy.studying.petclinic.model.Pet;
+import udemy.studying.petclinic.services.CrudService;
+
+import java.util.Set;
+
+public class PetServiceMap extends AbstractMapService<Pet, Long> implements CrudService<Pet, Long> {
+    @Override
+    public Set<Pet> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public Pet findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public Pet save(Pet pet) {
+        return super.save(pet.getId(), pet);
+    }
+
+    @Override
+    public void delete(Pet pet) {
+        super.delete(pet);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+}
