2730a340284f39d0593692574150e5c30bb8ae06
kmaznitsyn <maznichin03@gmail.com>
2022-12-18 21:22:55 +0200

    displaying list of Vets on vets/index page. closes #14.

--- pet-clinic-web/src/main/java/udemy/studying/petclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/udemy/studying/petclinic/controllers/VetController.java
@@ -1,15 +1,23 @@
 package udemy.studying.petclinic.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import udemy.studying.petclinic.services.VetService;
 
 @Controller
 @RequestMapping("/vets")
 public class VetController {
 
-    @RequestMapping({"","/", "/index", "/index.html"})
-    public String listVets() {
+    private final VetService vetService;
 
+    public VetController(VetService vetService) {
+        this.vetService = vetService;
+    }
+
+    @RequestMapping({"","/", "/index", "/index.html"})
+    public String listVets(Model model) {
+        model.addAttribute("vets", vetService.findAll());
         return "vets/index";
     }
 }
