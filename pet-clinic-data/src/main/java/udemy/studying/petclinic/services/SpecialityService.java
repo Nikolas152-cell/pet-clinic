720fa3f4038366c951d9f80e6dacb9b843a870c5
kmaznitsyn <maznichin03@gmail.com>
2023-01-04 21:54:41 +0200

    Added SpecialityService and SpecialityServiceMap. Closes #31.

---
+++ pet-clinic-data/src/main/java/udemy/studying/petclinic/services/SpecialityService.java
@@ -0,0 +1,6 @@
+package udemy.studying.petclinic.services;
+
+import udemy.studying.petclinic.model.vet.Speciality;
+
+public interface SpecialityService extends CrudService<Speciality, Long>{
+}
---
+++ pet-clinic-data/src/main/java/udemy/studying/petclinic/services/map/SpecialityServiceMap.java
@@ -0,0 +1,35 @@
+package udemy.studying.petclinic.services.map;
+
+import org.springframework.stereotype.Service;
+import udemy.studying.petclinic.model.vet.Speciality;
+import udemy.studying.petclinic.services.SpecialityService;
+
+import java.util.Set;
+
+@Service
+public class SpecialityServiceMap extends AbstractMapService<Speciality, Long> implements SpecialityService {
+    @Override
+    public Set<Speciality> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    public void delete(Speciality speciality) {
+        super.delete(speciality);
+    }
+
+    @Override
+    public Speciality save(Speciality speciality) {
+        return super.save(speciality);
+    }
+
+    @Override
+    public Speciality findById(Long id) {
+        return super.findById(id);
+    }
+}
