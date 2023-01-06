3a7a7fd3068aff7abffd32bd4249acbb621e1906
kmaznitsyn <maznichin03@gmail.com>
2023-01-06 21:05:02 +0200

    Impemented BaseEntity and NamedEntity for JPA. CLoses #35.

--- pet-clinic-data/src/main/java/udemy/studying/petclinic/model/NamedEntity.java
+++ pet-clinic-data/src/main/java/udemy/studying/petclinic/model/NamedEntity.java
@@ -1,6 +1,11 @@
 package udemy.studying.petclinic.model;
 
+import javax.persistence.Column;
+import javax.persistence.MappedSuperclass;
+
+@MappedSuperclass
 public class NamedEntity extends BaseEntity{
+    @Column(name = "name")
     private String name;
 
     public String getName() {
