d49690a8fc1727209a1b4b1c616301dede7f7c18
kmaznitsyn <maznichin03@gmail.com>
2023-01-08 18:06:39 +0200

    added circleci badge. Closes #59.

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # pet-clinic
 
+[![CircleCI](https://dl.circleci.com/status-badge/img/gh/kmaznitsyn/pet-clinic/tree/master.svg?style=svg)](https://dl.circleci.com/status-badge/redirect/gh/kmaznitsyn/pet-clinic/tree/master)
+
 pet-clinic
--- pet-clinic-web/src/test/java/udemy/studying/petclinic/PetClinicApplicationTests.java
+++ pet-clinic-web/src/test/java/udemy/studying/petclinic/PetClinicApplicationTests.java
@@ -1,7 +1,5 @@
 package udemy.studying.petclinic;
 
-import org.junit.Before;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
