bea8492e84b53ca754c55e95f3d8dd41a589a547
kmaznitsyn <maznichin03@gmail.com>
2023-01-16 14:44:26 +0200

    added Builder Pattern to PetType, and refactored a little NamedEntity. #55.

--- pet-clinic-data/src/main/java/udemy/studying/petclinic/model/NamedEntity.java
+++ pet-clinic-data/src/main/java/udemy/studying/petclinic/model/NamedEntity.java
@@ -1,6 +1,8 @@
 package udemy.studying.petclinic.model;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.Column;
@@ -9,6 +11,8 @@ import javax.persistence.MappedSuperclass;
 @MappedSuperclass
 @Getter
 @Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class NamedEntity extends BaseEntity{
     @Column(name = "name")
     private String name;
--- pet-clinic-data/src/main/java/udemy/studying/petclinic/model/pet/PetType.java
+++ pet-clinic-data/src/main/java/udemy/studying/petclinic/model/pet/PetType.java
@@ -1,15 +1,24 @@
 package udemy.studying.petclinic.model.pet;
 
-import lombok.Data;
+import lombok.*;
 import udemy.studying.petclinic.model.NamedEntity;
 
 import javax.persistence.Entity;
 import javax.persistence.Table;
 
-@Data
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 @Entity
 @Table(name = "types")
 public class PetType extends NamedEntity {
+
+    @Builder
+    public PetType(Long id, String name) {
+        this.setId(id);
+        this.setName(name);
+    }
 
     @Override
     public String toString() {
